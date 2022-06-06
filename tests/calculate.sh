14cfe09bcad78cf2a878489d7e018c79c8f5dcf6
Gavin Peng <gpeng@oicr.on.ca>
2022-06-06 10:30:52 -0400

    change calculate.sh command path

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 cd $1
 
 #find all fastq.gz files, return their md5sums to std out, list all file types
-find ./analysis/coverage/samtools_depth/ -name *.bed -xtype f -exec sh -c "cat {} | grep -v ^# | md5sum" \;
-find ./analysis/coverage/plots/ -name *.pdf -xtype f -exec sh -c "cat {} |md5sum" \;
-ls ./analysis/coverage/samtools_depth | sed 's/.*\.//' | sort | uniq -c
+find . -name *.bed -xtype f -exec sh -c "cat {} | grep -v ^# | md5sum" \;
+find . -name *.pdf -xtype f -exec sh -c "cat {} |md5sum" \;
+ls | sed 's/.*\.//' | sort | uniq -c
