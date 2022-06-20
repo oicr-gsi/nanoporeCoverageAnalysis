1818aa3106be23b6f63380070df2659ed6d04471
Gavin Peng <gpeng@oicr.on.ca>
2022-06-19 23:00:02 -0400

    minor change

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 #enter the workflow's final output directory ($1)
 cd $1
 
-#find all fastq.gz files, return their md5sums to std out, list all file types
-find  -name '*.bed' -xtype f -exec sh -c "cat {} | grep -v ^# | md5sum" \;
+#find all bed  files, return their md5sums 
+find  -name '*.bed' -xtype f -exec sh -c "cat {} | grep -v ^# | md5sum" 
